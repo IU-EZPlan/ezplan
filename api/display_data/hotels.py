2798a0dc795ad41245712df42ca984aa1dcec37e
cdvines <cdvines@iu.edu>
2021-11-14 13:19:26 -0500

    changes

--- api/display_data/hotels.py
+++ api/display_data/hotels.py
@@ -19,46 +19,6 @@ headers = {
 }
 
 
-
-def get_all():
-    conn.request("GET", "/v1/hotels/locations&locale=en-us", headers=headers)
-
-    res = conn.getresponse()
-    data = res.read()
-    # Turn into String
-    dataStr = data.decode()
-
-    # Turn this string into an object (in python, this is a dictionary aka json)
-    x = json.loads(dataStr)
-
-    # Get the destID and destType from the dictionary with US as the country
-    destID = ""
-    destType = ""
-    for d in x:
-        if d['country'] == 'United States':
-            destID = d["dest_id"]
-            destType = d["dest_type"]
-            break
-
-    # Make the new request
-    conn.request("GET", "/v1/hotels/search?locale=en-gb&room_number=1&checkout_date=2021-11-26&order_by=popularity&units=metric&adults_number=2&filter_by_currency=AED&checkin_date=2021-11-25&dest_type="+destType+"&dest_id="+destID+"&children_number=2&page_number=0&categories_filter_ids=facility%3A%3A107%2Cfree_cancellation%3A%3A1&children_ages=5%2C0", headers=headers)
-
-    # # Hotel Data!
-    res1 = conn.getresponse()
-    data1 = res1.read()
-
-    # Turn Into String
-    dataStr1 = data1.decode()
-
-
-    # Load the data and return it
-    # list_of_hotels = json.loads(dataStr1)
-
-    return dataStr1
-
-
-
-
 def get_hotels_by_location(location, adults_number, children_number, checkin_date, checkout_date, room_number):
 
     params = {}
@@ -74,7 +34,7 @@ def get_hotels_by_location(location, adults_number, children_number, checkin_dat
     if children_number:
         params['children_number'] = children_number
     else:
-        params['children_number'] = 0
+        params['children_number'] = 1 # children must be at lease 1, the api does not like 0 with each call
 
     if checkin_date:
         params['checkin_date'] = checkin_date
