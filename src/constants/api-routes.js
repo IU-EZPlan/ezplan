33dc8b2b1c2ab71f576881b0822d0b988171fab1
vevach <vevach@iu.edu>
2021-12-04 16:15:07 -0500

    changed BASE in api-routes.js

--- src/constants/api-routes.js
+++ src/constants/api-routes.js
@@ -1,4 +1,4 @@
-export const BASE = 'https://ezplan12-api.herokuapp.com';
+export const BASE = 'https://ezplan123.herokuapp.com';
 export const HOTELS = BASE + '/hotels';
 export const EVENTS = BASE + '/events';
 export const CURRENCY = BASE + "/exchange";
